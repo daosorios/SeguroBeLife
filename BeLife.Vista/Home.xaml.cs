8140102894224aadb88bf14ca6a0d8e7d7d8ee11
Daniela <da.osorios@alumnos.duoc.cl>
2020-04-10 14:42:03 -0400

    init

---
+++ BeLife.Vista/Home.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeLife.Vista
+{
+    /// <summary>
+    /// Lógica de interacción para Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public MainWindow ControladorHome;
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ControladorHome.miFrame.NavigationService.Navigate(ControladorHome.registrarCliente);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ControladorHome.miFrame.NavigationService.Navigate(ControladorHome.contrato);
+        }
+    }
+}

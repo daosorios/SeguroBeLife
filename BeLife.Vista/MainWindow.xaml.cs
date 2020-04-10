1e953086c50e0a0d2a1194f6e54eb50898be10b1
Daniela <da.osorios@alumnos.duoc.cl>
2020-04-10 14:42:03 -0400

    Mach con BD BeLife

---
+++ BeLife.Vista/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeLife.Vista
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Home home = new Home();
+        public RegistrarCliente registrarCliente = new RegistrarCliente();
+        public RegistrarContrato contrato = new RegistrarContrato();
+        public MainWindow()
+        {
+      
+            InitializeComponent();
+            home.ControladorHome = this; //para llamar la pagina desde otra pagina forma para que todas las pagina se comporten como el main
+            registrarCliente.ControladorCliente = this;
+            contrato.ControladorContrato = this;
+            
+
+            miFrame.NavigationService.Navigate(home);
+        }
+    }
+}

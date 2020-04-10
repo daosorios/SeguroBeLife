1ceb5e984275b6f9e81f2430c43043132410f79f
Daniela <da.osorios@alumnos.duoc.cl>
2020-04-10 15:18:52 -0400

    Inicio con botones abajo
    Crear 2 pagina para listar cliente y listar contrato
    vincular los botones a la pagina al hacer click

---
+++ BeLife.Vista/ListadoCliente.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeLife.Vista
+{
+    /// <summary>
+    /// Lógica de interacción para ListadoCliente.xaml
+    /// </summary>
+    public partial class ListadoCliente : Page
+    {
+        public MainWindow ControladorListadoCliente;//creando objeto controlador para que se comporte como el main
+        public ListadoCliente()
+        {
+            InitializeComponent();
+        }
+    }
+}

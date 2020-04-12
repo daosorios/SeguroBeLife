54e68350f16e25a145f392f76a443fecd7430701
Javier Sabando <ja.sabando@alumnos.duoc.cl>
2020-04-12 14:41:07 -0400

    comunicando la BD con la app(cliente)

--- SeguroBeLife/Cliente.cs
+++ SeguroBeLife/Cliente.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BeLife.Datos;
 
 //BD
 /*
@@ -38,6 +39,116 @@ namespace SeguroBeLife
             FechaNacimiento = DateTime.Today;
             IdSexo          =              0;
             IdEstadoCivil   =              0;
+        }
+
+        public bool Create()
+        {
+            BeLife.Datos.BeLifeEntities bbdd = new BeLife.Datos.BeLifeEntities();
+            BeLife.Datos.Cliente cli = new BeLife.Datos.Cliente();
+
+            try
+            {
+                Join.Syncronize(this, cli);
+                bbdd.Cliente.Add(cli);
+                bbdd.SaveChanges();
+
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                bbdd.Cliente.Remove(cli);
+                return false;
+            }
+
+        }
+
+        public bool Read()
+        {
+            BeLife.Datos.BeLifeEntities bbdd = new BeLife.Datos.BeLifeEntities();
+
+            try
+            {
+                BeLife.Datos.Cliente cli = bbdd.Cliente.First(e => e.RutCliente == RutCliente);
+                Join.Syncronize(cli, this);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public bool Update()
+        {
+            BeLife.Datos.BeLifeEntities bbdd = new BeLife.Datos.BeLifeEntities();
+            try
+            {
+
+                BeLife.Datos.Cliente cli = bbdd.Cliente.First(e => e.RutCliente == RutCliente);
+                Join.Syncronize(this, cli);
+
+                bbdd.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public bool Delete()
+        {
+            BeLife.Datos.BeLifeEntities bbdd = new BeLife.Datos.BeLifeEntities();
+
+            try
+            {
+                BeLife.Datos.Cliente cli = bbdd.Cliente.First(e => e.RutCliente == RutCliente);
+                bbdd.Cliente.Remove(cli);
+                bbdd.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public List<Cliente> ReadAll()
+        {
+            BeLife.Datos.BeLifeEntities bbdd = new BeLife.Datos.BeLifeEntities();
+
+            try
+            {
+                List<BeLife.Datos.Cliente> listaDatos = bbdd.Cliente.ToList<BeLife.Datos.Cliente>();
+
+                List<Cliente> listaRegistro = GenerarListado(listaDatos);
+
+                return listaRegistro;
+            }
+            catch (Exception ex)
+            {
+
+                return new List<Cliente>();
+            }
+        }
+
+        private List<Cliente> GenerarListado(List<BeLife.Datos.Cliente> listaDatos)
+        {
+            List<Cliente> listaRegistro = new List<Cliente>();
+
+            foreach (BeLife.Datos.Cliente datos in listaDatos)
+
+            {
+                Cliente cliente = new Cliente();
+                Join.Syncronize(datos, cliente);
+                listaRegistro.Add(cliente);
+
+            }
+            return listaRegistro;
         }
     }
 }
--- SeguroBeLife/Join.cs
+++ SeguroBeLife/Join.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using BeLife.Datos;
@@ -8,6 +9,23 @@ namespace SeguroBeLife
 {
     public class Join
     {
+        internal static void Syncronize(object origen, object destino)
+        {
+            Type tipo = null;
+            PropertyInfo[] propiedades = null;
+            tipo = origen.GetType();
+            propiedades = tipo.GetProperties();
 
+            foreach (PropertyInfo propiedad in propiedades)
+            {
+                try
+                {
+                    PropertyInfo propInfo = destino.GetType().GetProperty(propiedad.Name);
+
+                    propInfo.SetValue(destino, propiedad.GetValue(origen, null));
+                }
+                catch {/* Nel Bruhh*/}
+            }
+        }
     }
 }
